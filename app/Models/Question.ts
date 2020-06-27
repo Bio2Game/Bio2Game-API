80be8a31e7550df83fe0173e69701b9e09f5c8a2
DraftMan <nicovanaarsen@gmail.com>
2020-06-27 16:50:44 +0200

    Question model created

---
+++ app/Models/Question.ts
@@ -0,0 +1,49 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, belongsTo, BelongsTo, manyToMany, ManyToMany } from '@ioc:Adonis/Lucid/Orm'
+
+import Quiz from 'App/Models/Quiz'
+import User from 'App/Models/User'
+
+export default class Question extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public label: string
+
+  @column()
+  public profil: number
+
+  @column()
+  public question: string
+
+  @column()
+  public responses: JSON
+
+  @column()
+  public explication: string
+
+  @column()
+  public source: string
+
+  @column()
+  public status: number
+
+  @column()
+  public time: number
+
+  @column()
+  public language: string
+
+  @column()
+  public quizId: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @belongsTo(() => Quiz, { localKey: 'questionId', foreignKey: 'id' })
+  public quiz: BelongsTo<typeof Quiz>
+}
