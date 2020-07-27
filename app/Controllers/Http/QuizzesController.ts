8b86101a85172fc4be67a50b54c62c9204be3f9a
DraftMan <nicovanaarsen@gmail.com>
2020-07-27 20:48:07 +0200

    show, store, update, delete, validate quizzes

--- app/Controllers/Http/QuizzesController.ts
+++ app/Controllers/Http/QuizzesController.ts
@@ -15,51 +15,37 @@ export default class QuizzesController {
     return { success: true, quizzes }
   }
 
-  public async create ({ request, response}: HttpContextContract){
-    const validationSchema = schema.create({
-      label: schema.string({ trim: true }, [
-        rules.unique({ table: 'quizzes', column: 'label' }),
-        rules.maxLength(255),
-      ]),
-      description: schema.string({}, [
-        rules.maxLength(255),
-      ]),
-      status: schema.number(),
-      url: schema.string({ trim: true }, [
-        rules.regex(/^[-a-z0-9]+$/),
-        rules.maxLength(255),
-      ]),
-      contributorId: schema.number([
-        rules.exists({ table: 'users', column: 'id' }),
-      ]),
-      domainId: schema.number([
-        rules.exists({ table: 'domains', column: 'id' }),
-      ]),
-      language: schema.enum(Object.values(Languages)),
-      localisation: schema.string.optional({}, [
-        rules.maxLength(255),
-      ]),
-    })
+  public async show ({ response, params }: HttpContextContract){
+    try {
+      const quiz = await Quiz.find(params.id)
 
+      if(!quiz) {
+        response.status(404).json({ success: false })
+      }
+
+      return {
+        success: true,
+        quiz,
+      }
+    } catch (error) {
+      response.status(422).json({
+        success: false,
+        messages: error.messages,
+      })
+    }
+  }
+
+  public async store ({ request, response }: HttpContextContract){
     try {
       const payload = await request.validate({
-        schema: validationSchema,
-        messages: {
-          'label.required': 'Veuillez indiquer le nom du quiz.',
-          'label.unique': 'Ce nom est déjà utilisé par un autre quiz.',
-          'label.maxLength': 'Le nom de votre quiz ne peut pas dépasser {{ maxLength }} caractères.',
-          'description.required': 'Veuillez indiquer la description de votre quiz.',
-          'description.maxLength': 'La description de votre quiz ne peut pas dépasser {{ maxLength }} caractères.',
-          'status.required': 'Veuillez indiquer le status de votre quiz.',
-          'url.required': 'Veuillez indiquer l\'url de votre quiz.',
-          'url.regex': 'Veuillez respecter le format de l\'url.',
-          'url.maxLength': 'L\'url de votre quiz ne peut pas dépasser {{ maxLength }} caractères.',
-          'contributorId.required': 'Veuillez renseigner l\'id du contributeur.',
-          'contributorId.exists': 'Ce contributeur n\'existe pas.',
-          'domainId.required': 'Veuillez renseigner l\'id du domaine associé.',
-          'domainId.exists': 'Ce domaine n\'existe pas.',
-          'localisation.maxLength': 'Votre localisation ne peux pas dépasser {{ maxLength }} caractères.',
-        },
+        schema: schema.create({
+          label: schema.string({ trim: true }, [
+            rules.unique({ table: 'quizzes', column: 'label' }),
+            rules.maxLength(255),
+          ]),
+          ...this.validation.schema,
+        }),
+        messages: this.validation.messages,
       })
 
       //   payload.url = data.label.toLowerCase().normalize('NFKD')
@@ -72,11 +58,108 @@ export default class QuizzesController {
         quiz,
       }
     } catch (error) {
-      console.error(error)
       response.status(422).json({
         success: false,
         messages: error.messages,
       })
+    }
+  }
+
+  public async update ({ request, response, params }: HttpContextContract){
+    try {
+      const quiz = await Quiz.find(params.id)
+
+      if(!quiz) {
+        response.status(404).json({ success: false })
+      }
+
+      const payload = await request.validate({
+        schema: schema.create({
+          label: schema.string({ trim: true }, [
+            rules.unique({ table: 'quizzes', column: 'label', whereNot: { 'id': params.id } }),
+            rules.maxLength(255),
+          ]),
+          ...this.validation.schema,
+        }),
+        messages: this.validation.messages,
+      })
+
+      quiz?.merge(payload)
+
+      await quiz?.save()
+
+      return {
+        success: true,
+        quiz,
+      }
+    } catch (error) {
+      response.status(422).json({
+        success: false,
+        messages: error.messages,
+      })
+    }
+  }
+
+  public async delete ({ response, params }: HttpContextContract){
+    try {
+      const quiz = await Quiz.find(params.id)
+
+      if(!quiz) {
+        response.status(404).json({ success: false })
+      }
+
+      await quiz?.delete()
+
+      return {
+        success: true,
+        quiz,
+      }
+    } catch (error) {
+      response.status(422).json({
+        success: false,
+        messages: error.messages,
+      })
+    }
+  }
+
+  private get validation () {
+    return {
+      schema: {
+        description: schema.string({}, [
+          rules.maxLength(255),
+        ]),
+        status: schema.number(),
+        url: schema.string({ trim: true }, [
+          rules.regex(/^[-a-z0-9]+$/),
+          rules.maxLength(255),
+        ]),
+        contributorId: schema.number([
+          rules.exists({ table: 'users', column: 'id' }),
+        ]),
+        domainId: schema.number([
+          rules.exists({ table: 'domains', column: 'id' }),
+        ]),
+        language: schema.enum(Object.values(Languages)),
+        localisation: schema.string.optional({}, [
+          rules.maxLength(255),
+        ]),
+      },
+      messages: {
+        'label.required': 'Veuillez indiquer le nom du quiz.',
+        'label.unique': 'Ce nom est déjà utilisé par un autre quiz.',
+        'label.maxLength': 'Le nom de votre quiz ne peut pas dépasser {{ maxLength }} caractères.',
+        'description.required': 'Veuillez indiquer la description de votre quiz.',
+        'description.maxLength': 'La description de votre quiz ne peut pas dépasser {{ maxLength }} caractères.',
+        'status.required': 'Veuillez indiquer le status de votre quiz.',
+        'url.required': 'Veuillez indiquer l\'url de votre quiz.',
+        'url.regex': 'Veuillez respecter le format de l\'url.',
+        'url.maxLength': 'L\'url de votre quiz ne peut pas dépasser {{ maxLength }} caractères.',
+        'contributorId.required': 'Veuillez renseigner l\'id du contributeur.',
+        'contributorId.exists': 'Ce contributeur n\'existe pas.',
+        'domainId.required': 'Veuillez renseigner l\'id du domaine associé.',
+        'domainId.exists': 'Ce domaine n\'existe pas.',
+        'localisation.maxLength': 'Votre localisation ne peux pas dépasser {{ maxLength }} caractères.',
+      },
     }
   }
 }
