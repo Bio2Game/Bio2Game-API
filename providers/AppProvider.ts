7da78d9ec6a1ff3e6d970ad3fa53da65c95d77ce
DraftProducts <nicovanaarsen@gmail.com>
2020-10-02 00:43:19 +0200

    AppProvider created for Socket.io

--- providers/AppProvider.ts
+++ providers/AppProvider.ts
@@ -16,7 +16,15 @@ export default class AppProvider {
     // Cleanup, since app is going down
   }
 
-  public ready () {
-    // App is ready
+  public async ready () {
+    const App = await import('@ioc:Adonis/Core/Application')
+
+    /**
+     * Only import socket file, when environment is `web`. In other
+     * words do not import during ace commands.
+     */
+    if (App.default.environment === 'web') {
+      await import('../start/socket')
+    }
   }
 }
