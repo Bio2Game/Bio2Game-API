e03584a746be3cfe67bd5a1ec6dcee82fcc962dd
DraftProducts <nicovanaarsen@gmail.com>
2020-10-02 00:39:06 +0200

    name field removed from icons creation

--- app/Controllers/Http/IconsController.ts
+++ app/Controllers/Http/IconsController.ts
@@ -1,6 +1,6 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Application from '@ioc:Adonis/Core/Application'
-import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import { schema } from '@ioc:Adonis/Core/Validator'
 import Icon from 'App/Models/Icon'
 
 import lodash from 'lodash'
@@ -33,29 +33,26 @@ export default class IconsController {
 
   public async store ({ request, response}: HttpContextContract){
     const validationSchema = schema.create({
-      name: schema.string({ trim: true }, [
-        rules.maxLength(250),
-      ]),
-      icon: schema.file.optional({
+      icon: schema.file({
         size: '2mb',
         extnames: ['jpg', 'png', 'jpeg', 'svg'],
       }) as any,
     })
 
     try {
-      const { icon: image, name } = await request.validate({
+      const { icon: image } = await request.validate({
         schema: validationSchema,
         messages: {
-          'name.required': 'Veuillez indiquer le nom de l\'icone.',
-          'name.maxLength': 'Le nom de votre icone ne peut pas dépasser {{ maxLength }} caractères.',
           'icon.file.extname': 'Vous ne pouvez importer que des images',
           'icon.file.size': 'L\'image ne doit pas dépasser 2mo',
         },
       })
 
-      const iconName = `${lodash.snakeCase(name)}.${new Date().getTime()}.${image.subtype}`
+      console.log(image)
+
+      const iconName = `${lodash.snakeCase(image.fieldName)}.${new Date().getTime()}.${image.subtype}`
       await image.move(Application.publicPath('/images/icons'), { name: iconName })
-      const icon = await Icon.create({ reference: image.fileName })
+      const icon = await Icon.create({ reference: iconName })
 
       return {
         success: true,
