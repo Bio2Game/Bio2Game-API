2a46e6e61302bf075b90f604676d3510f6189924
DraftProducts <nicovanaarsen@gmail.com>
2020-10-02 00:41:17 +0200

    Party Model created

--- app/Models/Party.ts
+++ app/Models/Party.ts
@@ -5,6 +5,33 @@ export default class Party extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
+  @column()
+  public code: string
+
+  @column()
+  public name: string
+
+  @column()
+  public playersSize: number
+
+  @column()
+  public questionsList: string
+
+  @column()
+  public questionsSize: number
+
+  @column()
+  public quizId: number
+
+  @column()
+  public contributorId: number
+
+  @column()
+  public responses: string
+
+  @column()
+  public finished: boolean
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
