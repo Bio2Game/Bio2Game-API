c2857617a2f90bec6a79b992192f56e1d25feaf9
DraftProducts <nicovanaarsen@gmail.com>
2020-11-03 16:07:45 +0100

    Phase update for stats

--- socket.io/Phase.ts
+++ socket.io/Phase.ts
@@ -3,24 +3,31 @@ import Timer from './Timer'
 import Game from './Game'
 import Player from './Player'
 
-import { Question, PlayerResponsePayload } from './types'
+import { Question, PlayerResponsePayload, PlayerResponse } from './types'
 
 export default class Phase {
   public question: Question
   public timer: Timer
   private game: Game
 
-  constructor (question: Question, game: Game) {
+  private refs: string[]
+
+  public position: Number
+
+  constructor(question: Question, game: Game) {
     this.question = question
     this.game = game
+    this.position = this.game.questions.indexOf(question)
+
+    this.refs = ['right_answers', 'wrong_answers', 'wrong_answers', 'really_wrong_answers']
   }
 
-  public async run () {
+  public async run() {
     await this.start()
     return this.response()
   }
 
-  public async start () {
+  public async start() {
     this.timer = new Timer(this.question.time * 1000)
 
     this.game.room.emit('progress', {
@@ -31,20 +38,19 @@ export default class Phase {
     await this.timer.start()
   }
 
-  public async response () {
+  public async response() {
     this.timer = new Timer(10 * 1000)
 
-    this.game.party.responses = JSON.stringify(this.game.players.map((player: Player) => player.serialize()))
+    this.game.party.responses = JSON.stringify(this.game.players.map((player: Player) => player.asResponse()))
 
     this.game.party.save()
 
     this.game.onlinePlayers().forEach((player: Player) => {
-      player.socket.emit('showResponse', player.responses.find((response: PlayerResponsePayload) =>
+      player.socket.emit('show_response', player.responses.find((response: PlayerResponse) =>
         response.id === this.question.id))
     })
 
-    this.game.animator.socket.emit('playersResponses',
-      this.game.onlinePlayers().map((player: Player) => player.serialize()))
+    this.game.animator.socket.emit('game', this.game.serialize())
 
     this.game.room.emit('progress', {
       timeLeft: this.timer.getTimeLeft(),
@@ -56,24 +62,44 @@ export default class Phase {
     await this.timer.start()
   }
 
-  public onReponse (player: Player, data) {
+  public onReponse(player: Player, data: PlayerResponsePayload) {
     player.responses.push({
       id: data.id,
       response: data.response,
       time: this.timer.getCurrentTime(),
     })
 
+    this.saveAnswer(data)
+
+    if (this.game.pause) {
+      return
+    }
+
     if (this.game.onlinePlayers().length === this.game.players.filter((u: Player) =>
-      u.responses.some((response: PlayerResponsePayload) => response.id === data.id)
+      u.responses.some((response: PlayerResponse) => response.id === data.id)
     ).length) {
       this.timer.stop()
     }
   }
 
-  public serialize () {
+  private saveAnswer(data: PlayerResponsePayload) {
+    const question = this.game.stats.find(({ question_id }) => question_id === data.id)
+    question![this.refs[data.response.toString()]]++
+
+    this.game.animator.socket.emit('stats', this.game.stats)
+  }
+
+  public allAnswersReceived() {
+    return this.game.onlinePlayers().length === this.game.players.filter((u: Player) =>
+      u.responses.some((response: PlayerResponse) => response.id === this.question.id)
+    ).length
+  }
+
+  public serialize() {
     return {
       question: this.question,
       timer: this.timer?.serialize(),
+      position: this.position,
     }
   }
 }
