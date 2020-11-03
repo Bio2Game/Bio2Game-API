daa371107a1b4a64d9452f7f1417af6bcac892f1
DraftProducts <nicovanaarsen@gmail.com>
2020-11-03 16:08:00 +0100

    GameManager updated for new auth

--- socket.io/GameManager.ts
+++ socket.io/GameManager.ts
@@ -1,5 +1,3 @@
-// import AuthToken from 'App/Models/AuthToken'
-
 import { Server, Socket } from 'socket.io'
 
 import Game from './Game'
@@ -17,22 +15,22 @@ export default class GameManager {
   }
 
   public registerEvents (socket: Socket) {
-    socket.on('joinGame', async ({ gameId, token }: JoinGamePayload) => {
+    socket.on('joinGame', async ({ gameId, auth }: JoinGamePayload) => {
       const game = this.games.find(game => game.id === gameId)
 
-      if(!token) {
+      if(!auth || !auth.user) {
         socket.emit('authError', {
-          error: 404,
-          message: 'Non connecté',
+          error: 403,
+          message: 'Vous n\'êtes pas connecté !',
         })
-        console.log('Déconnexion auth !')
+        console.log('Bad auth !')
         return socket.disconnect()
       }
 
       if(!game) {
         socket.emit('gameError', {
           error: 404,
-          message: 'Impossible de trouver cette partie',
+          message: 'Il semblerait que cette partie n\'existe pas ou soit terminé !',
         })
         console.log('Déconnexion game !')
         return socket.disconnect()
@@ -45,7 +43,7 @@ export default class GameManager {
 
       socket.join(game.id)
 
-      await game.onConnect(socket, token)
+      return game.onConnect(socket, auth)
     })
 
     socket.on('createGame', async ({ name, questions, animatorId }: CreateGamePayload) => {
