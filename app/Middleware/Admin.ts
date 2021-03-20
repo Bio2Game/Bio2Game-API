24a2f898997bedd3b534e03be85c3399d8271817
DraftProducts <nicovanaarsen@gmail.com>
2021-03-21 00:34:41 +0100

    fix: 🐛 Fix status middleware

--- app/Middleware/Admin.ts
+++ app/Middleware/Admin.ts
@@ -1,12 +1,14 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { AuthenticationException } from '@adonisjs/auth/build/standalone'
 
+import User from 'App/Models/User'
+
 export default class Admin {
   public async handle ({ auth }: HttpContextContract, next: () => Promise<void>) {
     // code for middleware goes here. ABOVE THE NEXT CALL
-    if(auth.user?.status! < 2) {
-      /**
-     * Unable to authenticate using any guard
+    if(!(auth.user instanceof User && auth.user.status > 1)) {
+    /**
+     * Unable to authenticate as admin
      */
       throw new AuthenticationException(
         'Unauthorized access',
--- app/Middleware/Contributor.ts
+++ app/Middleware/Contributor.ts
@@ -1,12 +1,14 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { AuthenticationException } from '@adonisjs/auth/build/standalone'
 
+import User from 'App/Models/User'
+
 export default class Contributor {
   public async handle ({ auth }: HttpContextContract, next: () => Promise<void>) {
     // code for middleware goes here. ABOVE THE NEXT CALL
-    if(!auth.user?.status) {
+    if(!(auth.user instanceof User && auth.user.status)) {
       /**
-     * Unable to authenticate using any guard
+     * Unable to authenticate as contributor
      */
       throw new AuthenticationException(
         'Unauthorized access',
